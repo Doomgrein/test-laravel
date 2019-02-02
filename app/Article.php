7e7ea8c6f4fa506cf3ddc016b3ae75143bf070c3
Doomgrein <elwarrior@bk.ru>
2019-02-02 18:50:44 +0700

    Mismatch Exception

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use League\Flysystem\Config;
 
 class Article extends Model
 {
@@ -14,5 +15,27 @@ class Article extends Model
     public function users()
     {
         return $this->belongsToMany('App\User');
+    }
+
+    /**
+     * Функция, проверяющая, является ли автором статьи данный пользователь
+     * @param $user
+     * @return bool|mixed|null
+     */
+    public function isAuthor($user)
+    {
+        if (!$user) {
+            return Config::get('constants.mismatch');
+        }
+
+        if ($this->trashed()) {
+            return null;
+        }
+
+        if ($this->users()->find($user->id)) {
+            return true;
+        } else {
+            return Config::get('constants.mismatch');
+        }
     }
 }
